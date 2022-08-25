336fb0066ff2d86de88765fcd32addd422888d5d
Cyborg1999 <andrewk.tonui@gmail.com>
2022-08-25 17:42:50 +0300

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,17 +11,17 @@ size_t binary_tree_height2(const binary_tree_t *tree);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-size_t conf = 0, conf2 = 2, i = 0;
+	size_t conf = 0, conf2 = 2, i = 0;
 
-if (tree == NULL)
-return (0);
+	if (tree == NULL)
+		return (0);
 
-conf = binary_tree_height(tree);
-for (i = 1; i < conf; i++)
-conf2 = conf2 * 2;
-if (binary_tree_leaves(tree) == conf2)
-return (1);
-return (0);
+	conf = binary_tree_height(tree);
+	for (i = 1; i < conf; i++)
+		conf2 = conf2 * 2;
+		if (binary_tree_leaves(tree) == conf2)
+			return (1);
+		return (0);
 }
 
 /**
@@ -33,21 +33,21 @@ return (0);
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-int cont1 = 0;
+	int cont1 = 0;
 
-if (tree == NULL)
-return (0);
+	if (tree == NULL)
+		return (0);
 
-if (tree->left == NULL && tree->right == NULL)
-return (cont1 + 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (cont1 + 1);
 
-if (tree->left != NULL)
-cont1 += binary_tree_leaves(tree->left);
+	if (tree->left != NULL)
+		cont1 += binary_tree_leaves(tree->left);
 
-if (tree->right != NULL)
-cont1 += binary_tree_leaves(tree->right);
+	if (tree->right != NULL)
+		cont1 += binary_tree_leaves(tree->right);
 
-return (cont1);
+	return (cont1);
 }
 
 
@@ -61,20 +61,20 @@ return (cont1);
 
 size_t binary_tree_height2(const binary_tree_t *tree)
 {
-int cont1 = 0, cont2 = 0;
+	int cont1 = 0, cont2 = 0;
 
-if (tree == NULL)
-return (0);
+	if (tree == NULL)
+		return (0);
 
-if (tree->left != NULL)
-cont1 += binary_tree_height2(tree->left);
+	if (tree->left != NULL)
+		cont1 += binary_tree_height2(tree->left);
 
-if (tree->right != NULL)
-cont2 += binary_tree_height2(tree->right);
+	if (tree->right != NULL)
+		cont2 += binary_tree_height2(tree->right);
 
-if (cont1 > cont2)
-return (cont1 + 1);
-return (cont2 + 1);
+	if (cont1 > cont2)
+		return (cont1 + 1);
+	return (cont2 + 1);
 }
 
 /**
@@ -87,8 +87,8 @@ return (cont2 + 1);
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
+	if (tree == NULL)
+		return (0);
 
-return ((binary_tree_height2(tree)) - 1);
+	return ((binary_tree_height2(tree)) - 1);
 }
